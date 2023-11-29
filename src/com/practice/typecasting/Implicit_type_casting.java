edb8a03bd4fa7f439315b7a1c399d8b1238fc00c
Prajwal-Diwakar <prajwalkinnigoli59@gmail.com>
2023-11-29 10:28:30 +0530

    typecasting

---
+++ src/com/practice/typecasting/Implicit_type_casting.java
@@ -0,0 +1,23 @@
+package com.practice.typecasting;
+
+public class Implicit_type_casting {
+     public static void main(String[] args) {
+    	 //implicit type casting
+    	 //byte-short-int-long-float-double
+    	 
+    	 System.out.println("Implicit_type_casting");
+    	 
+    	 char a='P';
+    	 int b=a;
+    	 long c=a;
+    	 float d=a;
+    	 double e=a;
+    	 System.out.println("value of a =" +a);
+    			 System.out.println("value of b =" +b);
+    					 System.out.println("value of c =" +c);
+    							 System.out.println("value of d =" +d);
+    							 System.out.println("value of e =" +e);
+    	 
+    	 
+     }
+}
