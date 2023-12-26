135daf2ee7f779829d04fb2c98e013a85e752cad
Prajwal-Diwakar <88761865+Prajwal-Diwakar@users.noreply.github.com>
2023-12-26 18:23:47 +0530

    Add files via upload

---
+++ Phase2.learning-junit-maven-26-12-2023/src/main/java/Phase2/learning_junit_maven_26_12_2023/Phase2/learning_junit_maven_26_12_2023/App.java
@@ -0,0 +1,13 @@
+package Phase2.learning_junit_maven_26_12_2023.Phase2.learning_junit_maven_26_12_2023;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
