4ad513a70763232108aecce1a7a328e044bd332d
Prajwal-Diwakar <88761865+Prajwal-Diwakar@users.noreply.github.com>
2023-12-26 18:25:16 +0530

    Add files via upload

---
+++ Phase2.learning-selenium-junit5-26-12-2023/src/main/java/Phase2/learning_selenium_junit5_26_12_2023/App.java
@@ -0,0 +1,13 @@
+package Phase2.learning_selenium_junit5_26_12_2023;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
