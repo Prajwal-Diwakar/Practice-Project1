19bebdb42a763ed39ff028f7a25591e6d776cc87
Prajwal-Diwakar <88761865+Prajwal-Diwakar@users.noreply.github.com>
2023-12-26 22:59:25 +0530

    Add files via upload

---
+++ phase2.practiceProject4.JunitBrowserAutomation/src/main/java/phase2/practiceProject4/JunitBrowserAutomation/App.java
@@ -0,0 +1,13 @@
+package phase2.practiceProject4.JunitBrowserAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
